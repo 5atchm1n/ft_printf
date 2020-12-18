0ecfc43fa4bdcd181cb0f53c5f2f32c324328dfb
sshakya <sshakya@student.42.fr>
2020-12-18 04:19:06 +0100

    :construction: working ft_setflag
    *
    
    *

--- ft_setflags.c
+++ ft_setflags.c
@@ -6,18 +6,15 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 09:06:23 by sshakya           #+#    #+#             */
-/*   Updated: 2020/12/18 04:06:24 by sshakya          ###   ########.fr       */
+/*   Updated: 2020/12/18 04:18:02 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
+int			ft_isdigit(int c);
 
-
-int	ft_isdigit(int c);
-
-static int		ft_isflag(char c, char *flags)
+static int	ft_isflag(char c, char *flags)
 {
 	int		n;
 
@@ -30,12 +27,12 @@ static int		ft_isflag(char c, char *flags)
 			return (n);
 		n++;
 	}
-	return(-1);
+	return (-1);
 }
 
-static int		ft_fleft(char *str, char *flags)
+static int	ft_fleft(char *str, char *flags)
 {
-	int			i;
+	int		i;
 
 	i = 0;
 	while (*str != '\0' && i >= 0)
@@ -48,9 +45,9 @@ static int		ft_fleft(char *str, char *flags)
 	return (0);
 }
 
-static int		ft_fzero(char *str, char *flags)
+static int	ft_fzero(char *str, char *flags)
 {
-	int			i;
+	int		i;
 
 	i = 0;
 	while (*str != '\0' && i >= 0)
@@ -63,16 +60,16 @@ static int		ft_fzero(char *str, char *flags)
 	return (0);
 }
 
-static int		ft_fwidth(char *str, char *flags)
+static int	ft_fwidth(char *str, char *flags)
 {
-	int				i;
+	int		i;
 
 	i = 0;
 	while (*str && i >= 0)
 	{
 		i = ft_isflag(*str, flags);
 		if (i < 0)
-			break;
+			break ;
 		str++;
 	}
 	if (ft_isdigit(*str))
@@ -90,11 +87,9 @@ static int		ft_fwidth(char *str, char *flags)
 	return (0);
 }
 
-
-
-static int		ft_precision(char *str, char *flags)
+static int	ft_precision(char *str, char *flags)
 {
-	int			i;
+	int		i;
 
 	i = 0;
 	while (*str != '\0')
@@ -109,9 +104,9 @@ static int		ft_precision(char *str, char *flags)
 	return (0);
 }
 
-static int		ft_pwidth(char *str, char *flags)
+static int	ft_pwidth(char *str, char *flags)
 {
-	int				i;
+	int		i;
 
 	i = 0;
 	while (*str && i != 2)
@@ -119,7 +114,6 @@ static int		ft_pwidth(char *str, char *flags)
 		i = ft_isflag(*str, flags);
 		str++;
 	}
-	printf("%c\n", *str);	
 	if (ft_isdigit(*str))
 	{
 		i = 0;
@@ -134,9 +128,10 @@ static int		ft_pwidth(char *str, char *flags)
 	}
 	return (6);
 }
-t_flags			*ft_setflags(char *str, t_flags *flag)
+
+t_flags		*ft_setflags(char *str, t_flags *flag)
 {
-	char		*flags;
+	char	*flags;
 
 	flags = "-0.*";
 	str++;
