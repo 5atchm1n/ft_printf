60e05e9d1d288a6c3b43bf22b7652ff934ce672b
sshakya <sshakya@student.42.fr>
2021-01-06 20:50:27 +0100

    :construction: convert from number to str
    *
    
    *

--- srcs/pf_convert.c
+++ srcs/pf_convert.c
@@ -6,30 +6,76 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 11:13:33 by sshakya           #+#    #+#             */
-/*   Updated: 2021/01/06 11:53:39 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/01/06 20:42:16 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*pf_convert(int number, signed char format, t_flags flags)
+static char	*pf_convertbase(int num, char *base)
 {
-	char	*hex;
-	char	*HEX;
-	char	*decimal;
+	char	*ret;
+	int		i;
+	int		j;
+	int		len;
+
+	j = 0;
+	len = ft_strlen(base);
+	ret = malloc(sizeof(char) * 34);
+	while (num >= len)
+	{
+		i = num %  len;
+		ret[j] = base[i];
+		num = num / len;
+		j++;
+	}
+	ret[j] = base[num % len];
+	ret[j + 1] = '\0';
+	return (ret);
+}
+
+static char	*pf_revstr(char *str)
+{
+	int		len;
+	int		i;
+	int		j;
+	char	*ret;
+	
+	len = ft_strlen(str);
+	ret = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	j = len - 1;
+	while (i < len)
+	{
+		ret[i] = str[j];
+		i++;
+		j--;
+	}
+	ret[i] = '\0';
+	free(str);
+	return (ret);
+}
+char		*pf_convert(int number, signed char format, t_flags flags)
+{
 	char	*ret;
 
-	if (format == 'i')
+	if (format == 'i' || format == 'u' || format == 'd'))
 	{
-		pf_convertbase(number, flags);
+		ret = pf_convertbase(number, flags, "0123456789");
+		ret = pf_revstr(ret);
 		return (ret);
 	}
-	if (format == 'd')
+	
+	if (format == 'x')
 	{
-		pf_convertbase(number, flags);
+		ret = pf_converbase(number, flags, "0123456789abcdef");
+		ret = pf_revstr(ret);
 		return (ret);
 	}
-	if (format == 'u')
+	if (format == 'X')
 	{
-		pf_convertbase(number, flags);
+		ret = pf_convertbase(number, flags, "0123456789ABCDEF");
+		ret = pf_revstr(ret);
 		return (ret);
 	}
+	return (NULL);
+}
 
