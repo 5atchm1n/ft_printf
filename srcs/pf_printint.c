103efe01aea110ff3b34a4915227a7eaa372d695
sshakya <sshakya@student.42.fr>
2021-01-25 01:00:49 +0100

    :construction: macOS
    *
    
    *

--- srcs/pf_printint.c
+++ srcs/pf_printint.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 22:11:26 by sshakya           #+#    #+#             */
-/*   Updated: 2021/01/25 00:58:51 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/01/25 01:00:46 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ int					pf_printint(intmax_t num, t_flags flags, signed char format)
 	pfstring = pf_convert(n, format);
 	len = pf_strlen(pfstring);
 	if (flags.precision == 1 && (flags.pwidth == 0 || flags.pwidth == 1))
-		pfstring == NULL;
+		pfstring = NULL;
 	if (flags.precision == 1 && flags.pwidth > 0)
 	{
 		pfstring = pf_putzero(pfstring, flags.pwidth);
