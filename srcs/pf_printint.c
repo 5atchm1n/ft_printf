de97060a54fd72fcdc5600af290290ea8dc71d81
sshakya <sshakya@student.42.fr>
2021-01-26 15:47:28 +0100

    :construction: printint fix
    *
    
    *

--- srcs/pf_printint.c
+++ srcs/pf_printint.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 22:11:26 by sshakya           #+#    #+#             */
-/*   Updated: 2021/01/26 15:45:32 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/01/26 15:47:25 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,7 +103,7 @@ int					pf_printint(intmax_t num, t_flags flags, signed char format)
 	{
 		if (flags.zero == 1 && flags.fwidth > len && flags.left == 0)
 			pfstring = pf_putzero(pfstring, flags.fwidth);
-		pfstring = pf_addflags(pfstring, flags, neg, l);
+		pfstring = pf_addflags(pfstring, flags, neg, len);
 	}
 	len = pf_strlen(pfstring);
 	if (flags.fwidth > 0 && flags.fwidth > len)
