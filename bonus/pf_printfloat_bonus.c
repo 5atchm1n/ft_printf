6e8c2d6eda0df3d5b4cf07f995f9b23af3267581
sshakya <sshakya@student.42.fr>
2021-02-05 16:21:39 +0100

    :construction: final %f fix !
    *
    
    *

--- bonus/pf_printfloat_bonus.c
+++ bonus/pf_printfloat_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 01:53:08 by sshakya           #+#    #+#             */
-/*   Updated: 2021/02/05 15:37:52 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/02/05 16:21:16 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,22 +38,21 @@ static char		*pf_addflags(char *str, t_flags flags, int neg, int l)
 	if (neg == 1 && flags.zero == 1 && flags.fwidth > l)
 		str[0] = '-';
 //	if (neg == 0 && (flags.zero == 0 || flags.fwidth == l))
-	if (neg == 0)
+	if (neg == 0 && flags.zero == 0)
 	{
 		if ((flags.space == 1 && flags.plus == 1) || flags.plus == 1)
 			ret = pf_putflag(str, '+');
 		else if (flags.space == 1)
 			ret = pf_putflag(str, ' ');
 	}
-/*	
-	if (neg == 0 && flags.zero == 1 && flags.fwidth > l)
+	
+	if (neg == 0 && flags.zero == 1)
 	{
 		if ((flags.space == 1 && flags.plus == 1) || flags.plus == 1)
 			str[0] = '+';
 		else if (flags.space == 1)
 			str[0] = ' ';
 	}
-*/
 	return (ret);
 }
 
